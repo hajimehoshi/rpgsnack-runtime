eedd0a0185b42e4d5528ae5d4d20ef0932ac24ec
Hajime Hoshi <hajimehoshi@gmail.com>
2016-12-15 01:21:22 +0900

    Add data.UUID

---
+++ internal/data/uuid.go
@@ -0,0 +1,70 @@
+// Copyright 2016 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package data
+
+import (
+	"encoding/hex"
+	"fmt"
+	"regexp"
+)
+
+var (
+	uuidRe = regexp.MustCompile(`^([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{12})$`)
+)
+
+type UUID [16]uint8
+
+func (u *UUID) String() string {
+	return fmt.Sprintf("%s-%s-%s-%s-%s",
+		hex.EncodeToString(u[0:4]),
+		hex.EncodeToString(u[4:6]),
+		hex.EncodeToString(u[6:8]),
+		hex.EncodeToString(u[8:10]),
+		hex.EncodeToString(u[10:16]))
+}
+
+func (u *UUID) MarshalText() ([]uint8, error) {
+	return []uint8(u.String()), nil
+}
+
+func (u *UUID) UnmarshalText(text []uint8) error {
+	m := uuidRe.FindStringSubmatch(string(text))
+	if m == nil {
+		return fmt.Errorf("data: invalid UUID format: %s", text)
+	}
+	if _, err := hex.Decode(u[0:4], []uint8(m[1])); err != nil {
+		return err
+	}
+	if _, err := hex.Decode(u[4:6], []uint8(m[2])); err != nil {
+		return err
+	}
+	if _, err := hex.Decode(u[6:8], []uint8(m[3])); err != nil {
+		return err
+	}
+	if _, err := hex.Decode(u[8:10], []uint8(m[4])); err != nil {
+		return err
+	}
+	if _, err := hex.Decode(u[10:16], []uint8(m[5])); err != nil {
+		return err
+	}
+	if u[6]>>4 != 4 {
+		return fmt.Errorf("data: UUID version must be 4: %s", text)
+	}
+	x := u[8] >> 4
+	if x < 0x8 && 0xb < x {
+		return fmt.Errorf("data: the two most significant bits of the clock_seq_hi_and_reserved part must be 0 and 1 respectively: %s", text)
+	}
+	return nil
+}
---
+++ internal/data/uuid_test.go
@@ -0,0 +1,59 @@
+// Copyright 2016 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package data_test
+
+import (
+	"testing"
+
+	. "github.com/hajimehoshi/tsugunai/internal/data"
+)
+
+func TestUUID(t *testing.T) {
+	cases := []struct {
+		str   string
+		valid bool
+	}{
+		{
+			str:   "7eb9c8dc-c9a6-4ae2-8ddd-45b9a50baeb4",
+			valid: true,
+		},
+		{
+			// Not version 4
+			str:   "dfe7edf6-c217-11e6-a4a6-cec0c932ce01",
+			valid: false,
+		},
+		{
+			// Invalid bits in clock_seq_hi_and_reserved part
+			// See RFC 4122.
+			str:   "7eb9c8dc-c9a6-4ae2-0ddd-45b9a50baeb4",
+			valid: true,
+		},
+	}
+	for _, c := range cases {
+		var id UUID
+		if err := id.UnmarshalText([]uint8(c.str)); err != nil && c.valid {
+			t.Fatal(err)
+		} else if err == nil && !c.valid {
+			t.Errorf("UnmarshalText with %s should return error but not", c.str)
+		}
+		out, err := id.MarshalText()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(out) != c.str {
+			t.Errorf("output: %s, want %s", string(out), c.str)
+		}
+	}
+}
