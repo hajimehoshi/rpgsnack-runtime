d5236947ec2f62fdac7d09eab7ee0cfd347a3f48
wasedaigo <daigo.gamemaker@gmail.com>
2017-10-12 11:37:17 +0900

    Feature/footer (#313)

--- internal/ui/inventory.go
+++ internal/ui/inventory.go
@@ -39,23 +39,49 @@ type Inventory struct {
 	scrollX             int
 	dragX               int
 	scrolling           bool
+	autoScrolling       bool
+	pageIndex           int
+	targetPageIndex     int
+	bgPanel             *ImageView
+	frameCover          *ImageView
+	frameBase           *ImageView
+	activeCardSlot      *ImagePart
+	cardSlot            *ImagePart
+	activeDot           *ImagePart
+	dot                 *ImagePart
 }
 
 const (
-	buttonOffsetX       = 9
-	buttonOffsetY       = 6
-	frameXMargin        = 24
-	frameYMargin        = 4
-	itemXMargin         = 6
-	itemYMargin         = 6
-	itemSize            = 21
+	buttonOffsetX       = 0
+	buttonOffsetY       = 0
+	frameXMargin        = 34
+	frameYMargin        = 2
+	frameXPadding       = 6
+	frameYPadding       = 6
+	itemSize            = 19
 	scrollDragThreshold = 5
-	inventoryWidth      = 160
+	scrollBarWidth      = 120
+	itemPerPageCount    = 6
+	autoScrollSpeed     = 20
+	snapDragX           = 60
+	dotSpace            = 8
 )
 
 func NewInventory(x, y int) *Inventory {
-	button := NewImageButton(buttonOffsetX/consts.TileScale, (y+buttonOffsetY)/consts.TileScale, NewImagePart(assets.GetImage("system/active_item_box.png")), NewImagePart(assets.GetImage("system/active_item_box_pressed.png")), "click")
-	button.DisabledImage = NewImagePart(assets.GetImage("system/active_item_box_disabled.png"))
+	button := NewImageButton(
+		buttonOffsetX/consts.TileScale,
+		(y+buttonOffsetY)/consts.TileScale,
+		NewImagePartWithRect(assets.GetImage("system/ui_footer.png"), 40, 0, 36, 32),
+		NewImagePartWithRect(assets.GetImage("system/ui_footer.png"), 0, 0, 36, 32),
+		"click",
+	)
+	button.DisabledImage = NewImagePartWithRect(assets.GetImage("system/ui_footer.png"), 80, 0, 36, 32)
+
+	ty := y / consts.TileScale
+	bgPanel := NewImageView(0, ty, 1.0, NewImagePartWithRect(assets.GetImage("system/ui_footer.png"), 0, 32, 160, 40))
+	frameCover := NewImageView(34, ty+4, 1.0, NewImagePartWithRect(assets.GetImage("system/ui_footer.png"), 0, 144, 128, 24))
+	frameBase := NewImageView(34, ty+4, 1.0, NewImagePartWithRect(assets.GetImage("system/ui_footer.png"), 0, 168, 128, 24))
+
 	return &Inventory{
 		X:                   x,
 		Y:                   y,
@@ -64,12 +90,21 @@ func NewInventory(x, y int) *Inventory {
 		items:               []*data.Item{},
 		activeItemID:        0,
 		activeItemBoxButton: button,
+		bgPanel:             bgPanel,
+		frameCover:          frameCover,
+		frameBase:           frameBase,
+		cardSlot:            NewImagePartWithRect(assets.GetImage("system/ui_footer.png"), 120, 0, 18, 18),
+		activeCardSlot:      NewImagePartWithRect(assets.GetImage("system/ui_footer.png"), 138, 0, 18, 18),
+		dot:                 NewImagePartWithRect(assets.GetImage("system/ui_footer.png"), 120, 24, 8, 8),
+		activeDot:           NewImagePartWithRect(assets.GetImage("system/ui_footer.png"), 128, 24, 8, 8),
+		pageIndex:           0,
+		targetPageIndex:     0,
 	}
 }
 
 func (i *Inventory) slotIndexAt(x, y int) int {
-	x -= (frameXMargin + itemXMargin) * consts.TileScale
-	y = (y - (itemYMargin * consts.TileScale))
+	x -= (frameXMargin + frameXPadding) * consts.TileScale
+	y = (y - (frameYPadding * consts.TileScale))
 
 	if x >= 0 && i.Y <= y && y < i.Y+itemSize*consts.TileScale {
 		return x / (itemSize * consts.TileScale)
@@ -78,8 +113,20 @@ func (i *Inventory) slotIndexAt(x, y int) int {
 	return -1
 }
 
+func (i *Inventory) pageCount() int {
+	return int(math.Ceil(float64(len(i.items)) / float64(itemPerPageCount)))
+}
+
+func (i *Inventory) slotCount() int {
+	return i.pageCount() * itemPerPageCount
+}
+
 func (i *Inventory) ActiveItemPressed() bool {
 	return i.activeItemBoxButton.Pressed()
+}
+
+func (i *Inventory) calcScrollX(pageIndex int) int {
+	return -(pageIndex * (scrollBarWidth - frameXPadding)) * consts.TileScale
 }
 
 func (i *Inventory) Update() {
@@ -89,34 +136,32 @@ func (i *Inventory) Update() {
 		i.pressStartX = touchX
 		i.pressStartY = touchY
 		i.pressStartIndex = i.slotIndexAt(touchX-(i.scrollX+i.dragX), touchY)
+		i.autoScrolling = false
 	}
 	if input.Pressed() {
 		dx := touchX - i.pressStartX
 		if math.Abs(float64(dx)) > scrollDragThreshold && touchY > i.Y {
-			scrollBarWidth := inventoryWidth - frameXMargin
-			maxX := (itemXMargin + len(i.items)*itemSize - scrollBarWidth) * consts.TileScale
-
-			if maxX > 0 {
-				i.scrolling = true
-				i.dragX = dx
-				if i.scrollX+i.dragX > 0 {
-					i.dragX = -i.scrollX
-				}
-
-				if (i.scrollX + i.dragX) < -maxX {
-					i.dragX = -maxX - i.scrollX
-				}
-			}
+			i.scrolling = true
+			i.dragX = dx
 		}
 	}
 	if input.Released() {
-		if !i.scrolling && touchX > frameXMargin*consts.TileScale {
+		if !i.scrolling && touchX > (frameXMargin+frameXPadding)*consts.TileScale {
 			index := i.slotIndexAt(touchX-(i.scrollX+i.dragX), touchY)
 			if i.pressStartIndex == index {
 				i.PressedSlotIndex = index
 				i.pressStartIndex = -1
 			}
 		}
+
+		i.targetPageIndex = i.pageIndex
+		if i.dragX > snapDragX && i.pageIndex > 0 {
+			i.targetPageIndex = i.pageIndex - 1
+		}
+		if i.dragX < -snapDragX && i.pageIndex < i.pageCount()-1 {
+			i.targetPageIndex = i.pageIndex + 1
+		}
+		i.autoScrolling = true
 		i.scrollX += i.dragX
 		i.dragX = 0
 		i.scrolling = false
@@ -124,59 +169,97 @@ func (i *Inventory) Update() {
 
 	i.activeItemBoxButton.Update()
 	i.activeItemBoxButton.Disabled = i.activeItemID == 0
+
+	if i.autoScrolling {
+		targetX := i.calcScrollX(i.targetPageIndex)
+		dx := float64(targetX - i.scrollX)
+		if dx > 0 {
+			i.scrollX += autoScrollSpeed
+		} else {
+			i.scrollX -= autoScrollSpeed
+		}
+		if math.Abs(dx) < autoScrollSpeed {
+			i.scrollX = targetX
+			i.pageIndex = i.targetPageIndex
+			i.autoScrolling = false
+		}
+	}
 }
 
 func (i *Inventory) Draw(screen *ebiten.Image) {
 	if !i.Visible || len(i.items) == 0 {
 		return
 	}
+
+	i.bgPanel.Draw(screen)
+	i.frameBase.Draw(screen)
+
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Scale(consts.TileScale, consts.TileScale)
 	op.GeoM.Translate(float64(i.X+frameXMargin*consts.TileScale), float64(i.Y+frameYMargin*consts.TileScale))
-	screen.DrawImage(assets.GetImage("system/frame_inventory.png"), op)
 
 	var activeItem *data.Item
-	for index, item := range i.items {
-		if i.activeItemID == item.ID {
-			activeItem = item
+	for index := 0; index < i.slotCount(); index++ {
+		var item *data.Item
+		itemID := -2
+		if index < len(i.items) {
+			item = i.items[index]
+			itemID = item.ID
+			if i.activeItemID == item.ID {
+				activeItem = item
+			}
 		}
 
-		tx := float64((frameXMargin+itemXMargin+i.X+index*itemSize)*consts.TileScale + i.scrollX + i.dragX)
-		ty := float64(i.Y + itemYMargin*consts.TileScale)
+		tx := float64((frameXMargin+frameXPadding+i.X+index*itemSize)*consts.TileScale + i.scrollX + i.dragX)
+		ty := float64(i.Y+frameYPadding*consts.TileScale) + 2
 
-		if tx < frameXMargin || tx > inventoryWidth*consts.TileScale {
+		if tx < frameXMargin || tx > (frameXMargin+scrollBarWidth)*consts.TileScale {
 			continue
 		}
 
 		op := &ebiten.DrawImageOptions{}
 		op.GeoM.Scale(consts.TileScale, consts.TileScale)
 		op.GeoM.Translate(tx, ty)
-		if i.activeItemID == item.ID {
-			screen.DrawImage(assets.GetImage("system/card_frame_selected.png"), op)
+		if i.activeItemID == itemID {
+			i.activeCardSlot.Draw(screen, &op.GeoM, &ebiten.ColorM{})
 		} else {
-			screen.DrawImage(assets.GetImage("system/card_frame.png"), op)
+			i.cardSlot.Draw(screen, &op.GeoM, &ebiten.ColorM{})
 		}
-		op.GeoM.Translate(3, 3)
-		screen.DrawImage(assets.GetIconImage(item.Icon+".png"), op)
+
+		if item != nil {
+			op.GeoM.Translate(3, 0)
+			screen.DrawImage(assets.GetIconImage(item.Icon+".png"), op)
+		}
 	}
+
+	i.frameCover.Draw(screen)
 
 	i.activeItemBoxButton.Draw(screen)
 
 	if activeItem != nil {
 		dy := 0
 		if i.activeItemBoxButton.Pressing() {
-			dy = 3
+			dy = 6
 		}
 		op = &ebiten.DrawImageOptions{}
 		op.GeoM.Scale(consts.TileScale, consts.TileScale)
-		op.GeoM.Translate(float64(i.X+buttonOffsetX+14), float64(i.Y+buttonOffsetY+14+dy))
+		op.GeoM.Translate(float64(i.X+buttonOffsetX+30), float64(i.Y+buttonOffsetY+14+dy))
 		screen.DrawImage(assets.GetIconImage(activeItem.Icon+".png"), op)
-		if len(activeItem.Commands) > 0 {
-			op = &ebiten.DrawImageOptions{}
-			op.GeoM.Scale(consts.TileScale, consts.TileScale)
-			op.GeoM.Translate(float64(i.X+buttonOffsetX), float64(i.Y+buttonOffsetY+dy))
-			screen.DrawImage(assets.GetImage("system/item_box_info.png"), op)
+	}
+
+	centerX := frameXMargin + scrollBarWidth/2
+	left := int(float64(centerX) - float64(i.pageCount())/2*dotSpace)
+	for index := 0; index < i.pageCount(); index++ {
+		var imagePart *ImagePart
+		if index == i.pageIndex {
+			imagePart = i.activeDot
+		} else {
+			imagePart = i.dot
 		}
+		geoM := &ebiten.GeoM{}
+		geoM.Translate(float64(left+index*dotSpace), float64(i.Y/consts.TileScale+26))
+		geoM.Scale(consts.TileScale, consts.TileScale)
+		imagePart.Draw(screen, geoM, &ebiten.ColorM{})
 	}
 }
 
