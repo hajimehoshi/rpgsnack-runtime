b3c4dcf98e6bf5cb0e32ef5688c0bfd9ac4ed255
Hajime Hoshi <hajimehoshi@gmail.com>
2018-02-07 11:48:34 +0900

    font: Use font.Face

--- internal/font/font.go
+++ internal/font/font.go
@@ -15,127 +15,63 @@
 package font
 
 import (
-	"image"
 	"image/color"
+	"strings"
 
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/text"
+	"github.com/hajimehoshi/go-mplus-bitmap"
+	"golang.org/x/image/font"
 
-	"github.com/hajimehoshi/rpgsnack-runtime/internal/assets"
 	"github.com/hajimehoshi/rpgsnack-runtime/internal/data"
 )
 
-var positions map[rune]int
-
 const (
-	charHalfWidth       = 6
-	charFullWidth       = 12
-	charHeight          = 12
-	lineHeight          = 16
 	renderingLineHeight = 18
 )
 
-func runeWidth(r rune) int {
-	if r < 0x100 {
-		return charHalfWidth
-	}
-	return charFullWidth
-}
+var (
+	fonts = map[int]font.Face{}
+)
 
 func MeasureSize(text string) (int, int) {
 	w := 0
-	h := renderingLineHeight
-	cw := 0
-	for _, r := range text {
-		if r == '\n' {
-			if w < cw {
-				w = cw
-			}
-			cw = 0
-			h += renderingLineHeight
-			continue
-		}
-		cw += runeWidth(r)
-	}
-	if w < cw {
-		w = cw
+	h := 0
+	for _, l := range strings.Split(strings.TrimRight(text, "\n"), "\n") {
+		b, _ := font.BoundString(mplusbitmap.Gothic12r, l)
+		w += (b.Max.X - b.Min.X).Ceil()
+		h += renderingLineHeight
 	}
 	return w, h
 }
 
-func DrawText(screen *ebiten.Image, text string, ox, oy int, scale int, textAlign data.TextAlign, color color.Color, displayTextRuneCount int) {
-	if len(text) <= 0 {
-		return
+func DrawText(screen *ebiten.Image, str string, ox, oy int, scale int, textAlign data.TextAlign, color color.Color, displayTextRuneCount int) {
+	// Use the same instance to use text cache efficiently.
+	f, ok := fonts[scale]
+	if !ok {
+		f = Scale(mplusbitmap.Gothic12r, scale)
+		fonts[scale] = f
 	}
 
-	if positions == nil {
-		positions = map[rune]int{}
-		b := assets.GetResource("images/fonts/mplus_positions")
-		for i := 0; i < len(b)/4; i++ {
-			r := rune(b[4*i]) + rune(b[4*i+1])<<8
-			pos := int(b[4*i+2]) + int(b[4*i+3])<<8
-			positions[r] = pos
-		}
-	}
-	op := &ebiten.DrawImageOptions{}
-	r, g, b, a := color.RGBA()
-	op.ColorM.Scale(float64(r)/65535, float64(g)/65535, float64(b)/65535, float64(a)/65535)
-
-	x := 0
-	lineWidths := []int{}
-	for _, r := range text {
-		if r == '\n' {
-			lineWidths = append(lineWidths, x)
-			x = 0
-			continue
-		}
-		x += runeWidth(r)
-	}
-	lineWidths = append(lineWidths, x)
-
-	dx := 0
-	dy := (renderingLineHeight - lineHeight) / 2
-	l := 0
-	img := assets.GetImage("fonts/mplus.compacted.png")
-	displayText := []rune(text)[0:displayTextRuneCount]
-
-	for _, r := range displayText {
-		if r == '\n' {
-			dx = 0
-			dy += renderingLineHeight
-			l++
-			continue
-		}
-		// TODO: Use unicode package to detect space
-		if r == ' ' || r == '　' {
-			dx += runeWidth(r)
-			continue
-		}
-		pos, ok := positions[r]
-		if !ok {
-			continue
-		}
-		x := pos % 256 * charFullWidth
-		y := pos / 256 * lineHeight
-		w := runeWidth(r)
-		h := lineHeight
-		src := image.Rect(x, y, x+w, y+h)
-		op.SourceRect = &src
-
-		lw := lineWidths[l]
-		op.GeoM.Reset()
+	b, _, _ := f.GlyphBounds('.')
+	dotX := -b.Min.X
+	dotY := -b.Min.Y
+	for _, l := range strings.Split(str, "\n") {
+		x := ox + dotX.Floor()
+		y := oy + dotY.Floor()
+		_, a := font.BoundString(f, l)
 		switch textAlign {
 		case data.TextAlignLeft:
-			op.GeoM.Translate(float64(dx), float64(dy))
+			// do nothing
 		case data.TextAlignCenter:
-			op.GeoM.Translate(float64(dx-lw/2), float64(dy))
+			x -= a.Ceil() / 2
 		case data.TextAlignRight:
-			op.GeoM.Translate(float64(dx-lw), float64(dy))
+			x -= a.Ceil()
 		default:
 			panic("not reached")
 		}
-		op.GeoM.Scale(float64(scale), float64(scale))
-		op.GeoM.Translate(float64(ox), float64(oy))
-		screen.DrawImage(img, op)
-		dx += w
+
+		text.Draw(screen, l, f, x, y, color)
+		oy += renderingLineHeight * scale
 	}
 }
---
+++ internal/font/scaled.go
@@ -0,0 +1,116 @@
+// Copyright 2018 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package font
+
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+)
+
+type scaledImage struct {
+	image image.Image
+	scale int
+}
+
+func (s *scaledImage) ColorModel() color.Model {
+	return s.image.ColorModel()
+}
+
+func (s *scaledImage) Bounds() image.Rectangle {
+	b := s.image.Bounds()
+	b.Min = b.Min.Mul(s.scale)
+	b.Max = b.Max.Mul(s.scale)
+	return b
+}
+
+func euclidianDiv(x, y int) int {
+	if x < 0 {
+		x -= y - 1
+	}
+	return x / y
+}
+
+func (s *scaledImage) At(x, y int) color.Color {
+	x = euclidianDiv(x, s.scale)
+	y = euclidianDiv(y, s.scale)
+	return s.image.At(x, y)
+}
+
+func Scale(f font.Face, scale int) font.Face {
+	if scale == 1 {
+		return f
+	}
+	return &scaled{f, scale}
+}
+
+type scaled struct {
+	font  font.Face
+	scale int
+}
+
+func (s *scaled) Close() error {
+	return s.font.Close()
+}
+
+func (s *scaled) Glyph(dot fixed.Point26_6, r rune) (dr image.Rectangle, mask image.Image, maskp image.Point, advance fixed.Int26_6, ok bool) {
+	dr, mask, maskp, advance, ok = s.font.Glyph(dot, r)
+	if !ok {
+		return
+	}
+	d := image.Pt(dot.X.Floor(), dot.Y.Floor())
+	dr.Min = dr.Min.Sub(d).Mul(s.scale).Add(d)
+	dr.Max = dr.Max.Sub(d).Mul(s.scale).Add(d)
+	maskp = maskp.Mul(s.scale)
+	advance *= fixed.Int26_6(s.scale)
+	return dr, &scaledImage{mask, s.scale}, maskp, advance, ok
+}
+
+func (s *scaled) GlyphBounds(r rune) (bounds fixed.Rectangle26_6, advance fixed.Int26_6, ok bool) {
+	bounds, advance, ok = s.font.GlyphBounds(r)
+	if !ok {
+		return
+	}
+	bounds.Min.X *= fixed.Int26_6(s.scale)
+	bounds.Min.Y *= fixed.Int26_6(s.scale)
+	bounds.Max.X *= fixed.Int26_6(s.scale)
+	bounds.Max.Y *= fixed.Int26_6(s.scale)
+	advance *= fixed.Int26_6(s.scale)
+	return
+}
+
+func (s *scaled) GlyphAdvance(r rune) (advance fixed.Int26_6, ok bool) {
+	advance, ok = s.font.GlyphAdvance(r)
+	if !ok {
+		return
+	}
+	advance *= fixed.Int26_6(s.scale)
+	return
+}
+
+func (s *scaled) Kern(r0, r1 rune) fixed.Int26_6 {
+	return s.font.Kern(r0, r1) * fixed.Int26_6(s.scale)
+}
+
+func (s *scaled) Metrics() font.Metrics {
+	m := s.font.Metrics()
+	return font.Metrics{
+		Height:  m.Height * fixed.Int26_6(s.scale),
+		Ascent:  m.Ascent * fixed.Int26_6(s.scale),
+		Descent: m.Descent * fixed.Int26_6(s.scale),
+	}
+}
