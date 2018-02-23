6d0d05bfb8df31bf6019d1f0a24f8a99f41430fe
Zero Liu <zerolxy@gmail.com>
2018-02-22 23:30:34 -0800

    add chinese translation (#440)

--- internal/texts/texts.go
+++ internal/texts/texts.go
@@ -105,59 +105,58 @@ and connect to the network`,
 確認してください`,
 	},
 	language.SimplifiedChinese: {
-		// TODO: Translat this
-		TextIDNewGame:    "New Game",
-		TextIDResumeGame: "Resume Game",
-		TextIDYes:        "Yes",
-		TextIDNo:         "No",
+		TextIDNewGame:    "新游戏",
+		TextIDResumeGame: "继续游戏",
+		TextIDYes:        "确定",
+		TextIDNo:         "取消",
 		TextIDOK:         "OK",
-		TextIDSettings:   "Settings",
-		TextIDLanguage:   "Language",
-		TextIDCredit:     "Credit",
-		TextIDRemoveAds:  "Remove Ads",
-		TextIDRemoveAdsDesc: `Would you like to pay %s
-to remove ads from the game?`,
-		TextIDReviewThisApp:    "Review This App",
-		TextIDRestorePurchases: "Restore Purchases",
-		TextIDMoreGames:        "More Games",
-		TextIDClose:            "Close",
-		TextIDItemCheck:        "Check",
+		TextIDSettings:   "设定",
+		TextIDLanguage:   "语言",
+		TextIDCredit:     "制作人员",
+		TextIDRemoveAds:  "移除广告",
+		TextIDRemoveAdsDesc: `你希望支付%s
+来移除游戏里的广告吗?`,
+		TextIDReviewThisApp:    "点评我们的游戏",
+		TextIDRestorePurchases: "恢复购买",
+		TextIDMoreGames:        "更多游戏",
+		TextIDClose:            "关闭",
+		TextIDItemCheck:        "查看",
 
-		TextIDNewGameWarning: `You have a on-going game data.
-Do you want to clear the
-progress to start a new game?`,
-		TextIDBackToTitle: "Do you want to go back to title?",
-		TextIDQuitGame:    "Do you want to quit the game?",
-		TextIDStoreError: `Failed to connect to the store.
-Please make sure to sign in
-and connect to the network`,
+		TextIDNewGameWarning: `中断存档已存在
+你想清除这个存档
+并开始新游戏吗?`,
+		TextIDBackToTitle: "返回主菜单?",
+		TextIDQuitGame:    "退出游戏?",
+		TextIDStoreError: `无法连接商店
+请确定你已经登录
+并已连上网络`,
 	},
 	language.TraditionalChinese: {
 		// TODO: Translat this
-		TextIDNewGame:    "New Game",
-		TextIDResumeGame: "Resume Game",
-		TextIDYes:        "Yes",
-		TextIDNo:         "No",
+		TextIDNewGame:    "新遊戲",
+		TextIDResumeGame: "繼續遊戲",
+		TextIDYes:        "確定",
+		TextIDNo:         "取消",
 		TextIDOK:         "OK",
-		TextIDSettings:   "Settings",
-		TextIDLanguage:   "Language",
-		TextIDCredit:     "Credit",
-		TextIDRemoveAds:  "Remove Ads",
-		TextIDRemoveAdsDesc: `Would you like to pay %s
-to remove ads from the game?`,
-		TextIDReviewThisApp:    "Review This App",
-		TextIDRestorePurchases: "Restore Purchases",
-		TextIDMoreGames:        "More Games",
-		TextIDClose:            "Close",
-		TextIDItemCheck:        "Check",
+		TextIDSettings:   "設定",
+		TextIDLanguage:   "語言",
+		TextIDCredit:     "製作人員",
+		TextIDRemoveAds:  "移除廣告",
+		TextIDRemoveAdsDesc: `你希望支付%s
+來移除遊戲裡的廣告嗎?`,
+		TextIDReviewThisApp:    "點評我們的遊戲",
+		TextIDRestorePurchases: "恢復購買",
+		TextIDMoreGames:        "更多遊戲",
+		TextIDClose:            "關閉",
+		TextIDItemCheck:        "查看",
 
-		TextIDNewGameWarning: `You have a on-going game data.
-Do you want to clear the
-progress to start a new game?`,
-		TextIDBackToTitle: "Do you want to go back to title?",
-		TextIDQuitGame:    "Do you want to quit the game?",
-		TextIDStoreError: `Failed to connect to the store.
-Please make sure to sign in
-and connect to the network`,
+		TextIDNewGameWarning: `中斷存檔已存在
+你想清除這個存檔
+並開始新遊戲嗎?`,
+		TextIDBackToTitle: "返回主菜單?",
+		TextIDQuitGame:    "退出遊戲?",
+		TextIDStoreError: `無法連接商店
+請確定你已經登錄
+並已連上網絡`,
 	},
 }
