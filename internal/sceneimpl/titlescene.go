8653c1add7f95ce2e4619e250c1627c14584a5a1
Daigo Sato <daigo.gamemaker@gmail.com>
2018-06-02 15:26:49 -0700

    Adjusted removeAdsButton layout

--- internal/sceneimpl/titlescene.go
+++ internal/sceneimpl/titlescene.go
@@ -77,7 +77,7 @@ func (t *TitleScene) initUI(sceneManager *scene.Manager) {
 	t.resumeGameButton = ui.NewTextButton((w/consts.TileScale-120)/2, h/consts.TileScale-by-32, 120, 20, "click")
 	t.titleLine = ui.NewImageView((w/consts.TileScale-120)/2+20, h/consts.TileScale-by-32, 1.0, ui.NewImagePart(assets.GetImage("system/common/title_line.png")))
 	t.newGameButton = ui.NewTextButton((w/consts.TileScale-120)/2, h/consts.TileScale-by-52, 120, 20, "click")
-	t.removeAdsButton = ui.NewTextButton((w/consts.TileScale-120)/2, h/consts.TileScale-by-8, 120, 20, "click")
+	t.removeAdsButton = ui.NewTextButton((w/consts.TileScale-120)/2+20, h/consts.TileScale-by-8, 80, 20, "click")
 	t.removeAdsButton.TextColor = color.RGBA{0xc8, 0xc8, 0xc8, 0xff}
 	t.settingsButton = ui.NewImageButton(w/consts.TileScale-24, h/consts.TileScale-by, settingsIcon, settingsIcon, "click")
 	t.moregamesButton = ui.NewImageButton(12, h/consts.TileScale-by, moreGamesIcon, moreGamesIcon, "click")
